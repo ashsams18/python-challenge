d1f9f6225b7859528858fd2a4f812901765462e3
Arshad <ashsams@hotmail.com>
2021-11-14 22:25:23 +0000

    14Nov2021-1

---
+++ PyBank/main.py
@@ -0,0 +1,56 @@
+# Your task is to create a Python script that analyzes the records to calculate each of the following:
+import os
+import csv
+
+file_path = os.path.join(".", "Resources", "budget_data.csv")
+num_rows = 0
+net_total = 0
+current = 0
+previous = 0
+change = 0
+sum_all_changes = 0
+count_all_changes = 0
+should_count = False
+
+with open(file_path, 'r') as csv_file:
+  csvreader = csv.reader(csv_file, delimiter=",")
+  header = next(csvreader)
+
+# The total number of months included in the dataset
+  for row in csvreader:
+    num_rows += 1
+    # The net total amount of "Profit/Losses" over the entire period
+    # row[1]
+    net_total = int(row[1])+net_total
+    print(type(row[1]))
+    # current = int(row[1])
+    
+    # change = current - previous
+    # previous = current
+    
+    # if should_count:
+    #   #print("Summing", change)
+    #   sum_all_changes += change
+    #   count_all_changes += 1
+    # else:
+    #   should_count = True
+
+# Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
+# Average_Change = change /num_rows
+# The greatest increase in profits (date and amount) over the entire period
+
+# The greatest decrease in profits (date and amount) over the entire period
+
+# text
+  # Financial Analysis
+  # Total Months: 86
+  print (f"Total Months: {num_rows}")
+  # Total: $38382578
+  print(f"Total : ${net_total}")
+  # # Average  Change: $-2315.12
+  # print(f"Average Change : {net_total}")
+  # # Greatest Increase in Profits: Feb-2012 ($1926159)
+  # print(f"Greatest Increase in Profits : {net_total}")
+  # # Greatest Decrease in Profits: Sep-2013 ($-2196167)
+  # print(f"Greatest Decrease in Profits : {net_total}")
+# In addition, your final script should both print the analysis to the terminal and export a text file with the results.
