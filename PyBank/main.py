efd239440532590187c7eb151eff9fff3e45450a
Arshad <ashsams@hotmail.com>
2021-11-15 21:01:14 +0000

    15Nov -1

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,6 +11,8 @@ change = 0
 sum_all_changes = 0
 count_all_changes = 0
 should_count = False
+greatest_value = 0
+greatest_month = ""
 
 with open(file_path, 'r') as csv_file:
   csvreader = csv.reader(csv_file, delimiter=",")
@@ -22,21 +24,25 @@ with open(file_path, 'r') as csv_file:
     # The net total amount of "Profit/Losses" over the entire period
     # row[1]
     net_total = int(row[1])+net_total
-    print(type(row[1]))
-    # current = int(row[1])
+    # print(type(row[1]))
+    current = int(row[1])
     
-    # change = current - previous
-    # previous = current
+    change = current - previous
+    previous = current
     
-    # if should_count:
-    #   #print("Summing", change)
-    #   sum_all_changes += change
-    #   count_all_changes += 1
-    # else:
-    #   should_count = True
+    if should_count:
+      #print("Summing", change)
+      sum_all_changes += change
+      count_all_changes += 1
+    else:
+      should_count = True
+    
+    if change > greatest_value:
+      greatest_value = change
+      greatest_month = row[0]
 
 # Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
-# Average_Change = change /num_rows
+
 # The greatest increase in profits (date and amount) over the entire period
 
 # The greatest decrease in profits (date and amount) over the entire period
@@ -48,9 +54,9 @@ with open(file_path, 'r') as csv_file:
   # Total: $38382578
   print(f"Total : ${net_total}")
   # # Average  Change: $-2315.12
-  # print(f"Average Change : {net_total}")
+  print(f"Average Change : ${round(sum_all_changes / count_all_changes, 2)}")
   # # Greatest Increase in Profits: Feb-2012 ($1926159)
-  # print(f"Greatest Increase in Profits : {net_total}")
+  print(f"Greatest Increase in Profits : {greatest_month} | ${greatest_value}")
   # # Greatest Decrease in Profits: Sep-2013 ($-2196167)
   # print(f"Greatest Decrease in Profits : {net_total}")
 # In addition, your final script should both print the analysis to the terminal and export a text file with the results.
